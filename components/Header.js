a7659b6571d6c77ccdb4fcd21238f381d38a6b1f
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-08-08 21:39:31 +0530

    Built right side of the Header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { SearchIcon } from "@heroicons/react/solid"
+import { GlobeAltIcon, MenuIcon, SearchIcon, UserCircleIcon } from "@heroicons/react/solid"
 
 const Header = () => {
     return (
@@ -20,9 +20,16 @@ const Header = () => {
                 <input className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400" type="text" placeholder="Start your search" />
                 <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
             </div>
-            {/* right */}
-            <div>
 
+            {/* right */}
+            <div className="flex items-center space-x-4 justify-end text-gray-500">
+                <p className="hidden md:inline cursor-pointer">Become a host</p>
+                <GlobeAltIcon className="h-6 cursor-pointer" />
+
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full ">
+                    <MenuIcon className="h-6 cursor-pointer" />
+                    <UserCircleIcon className="h-6 cursor-pointer" />
+                </div>
             </div>
 
 
