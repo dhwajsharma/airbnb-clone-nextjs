4d506c74521c0a51ef6dd0fb9be020621ae5e460
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-08-10 22:02:57 +0530

    Built Map Component

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
         domains: ["links.papareact.com"]
+    },
+    env: {
+        mapbox_key: "pk.eyJ1IjoiZGh3YWpzaGFybWEiLCJhIjoiY2tzNWxpMGEzMnl5bDJwczd0bDdiMXZkYSJ9.nLGVqWqhXQB3jTAxXiS83g"
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home({ exploreData, cardsData }) {
       <Header />
       <Banner />
 
-      <main className="max-w-7xl mx-auto px-8 sm:px-16 ">
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
