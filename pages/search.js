bfcc900434683d89c439927ca6fb40c5e6e78b92
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-08-10 22:44:58 +0530

    fixed a bug

--- pages/search.js
+++ pages/search.js
@@ -20,7 +20,7 @@ const Search = ({ searchResults }) => {
 
             <main className=" flex">
                 <section className="flex-grow pt-14 px-6">
-                    <p className="text-xs">300+ Stays - {range} - for {noOfGuests} guests</p>
+                    <p className="hidden md:inline-flex text-xs">300+ Stays - {range} - for {noOfGuests} guests</p>
                     <h1 className="text-3xl font-semibold mt-2 mb-6" >Stays in {location}</h1>
 
                     <div className="dark:text-white hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
